30f58f36b74c6f754a898370d7d6b4a91364c198
yudiaguspratama-netizen <yudiaguspratama@gmail.com>
2026-04-26 19:43:04 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,1 +1,20 @@
-sed -i 's/SNAPSHOT/23.05.2/g' feeds.conf.default
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# 1. Tambahkan feed src-git untuk aplikasi tambahan (Opsional)
+# Ini berguna jika Anda ingin paket yang lebih lengkap dari komunitas
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+
+# 2. Pastikan feed bawaan ImmortalWrt aktif
+# Biasanya sudah otomatis, tapi baris ini memastikan tidak ada yang terkomentar
+sed -i 's/^#\(src-git custom\)/\1/' feeds.conf.default
